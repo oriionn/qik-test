8eaa18074b6168e026cf3775277471deaff0c008
oriionn <xemilien67@gmail.com>
2023-08-29 16:36:12 +0200

    Add the rest of function

--- v2/app.js
+++ v2/app.js
@@ -1,5 +1,5 @@
 const pkg = require("./package.json");
 
-console.log("App V1")
+console.log("App V2")
 if (pkg.name !== "qik-test") console.log("Name OK.")
 if (pkg.description !== "Test for Qik") console.log("Description OK.")
